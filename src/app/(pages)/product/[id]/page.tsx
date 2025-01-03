2af6a80da95bd0fc3117ca48fc69bcbf723327fa
ManaTeWai <maksimts03@gmail.com>
2025-01-03 03:01:34 +0300

    +

--- src/app/(pages)/product/[id]/page.tsx
+++ src/app/(pages)/product/[id]/page.tsx
@@ -17,7 +17,8 @@ type ProductPageProps = {
 	};
 };
 
-export async function generateMetadata({ params }: Awaited<ProductPageProps>): Promise<Metadata> {
+// Функция для генерации метаданных
+export async function generateMetadata({ params }: ProductPageProps): Promise<Metadata> {
 	const productId = parseInt(params.id, 10);
 
 	const { data: product, error } = await supabase
@@ -32,8 +33,8 @@ export async function generateMetadata({ params }: Awaited<ProductPageProps>): P
 	};
 }
 
-export default async function ProductPage(props: Promise<ProductPageProps>) {
-	const { params } = await props; // Обработка `Promise`
+// Основной экспорт страницы
+export default async function ProductPage({ params }: ProductPageProps) {
 	const productId = parseInt(params.id, 10);
 
 	const { data: product, error } = await supabase
@@ -50,19 +51,17 @@ export default async function ProductPage(props: Promise<ProductPageProps>) {
 		<div className={styles.page_wrapper}>
 			<div className={styles.main_content}>
 				<div className={styles.title}>
-					<Htag tag="h1" className={styles.titleHeader}>{product.title}</Htag>
+					<Htag tag='h1' className={styles.titleHeader}>{product.title}</Htag>
 					<Image src={product.imageUrl} alt={product.title} width={250} height={250} />
 				</div>
 				<div className={styles.description}>
-					<P size="medium">{product.description}</P>
-					<P size="large">
-						Цена: <span className={styles.price}>{product.price} РУБ.</span>
-					</P>
+					<P size='medium'>{product.description}</P>
+					<P size='large'>Цена: <span className={styles.price}>{product.price} РУБ.</span></P>
 					<RatingState productId={productId} />
 				</div>
 			</div>
 			<div className={styles.some_desc}>
-				<P size="medium">{product.some_desc}</P>
+				<P size='medium'>{product.some_desc}</P>
 			</div>
 		</div>
 	);
