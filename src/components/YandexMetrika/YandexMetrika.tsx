1af3f66e95d01b612e699f2906ad2f93cd5b367a
Mana Nedau <maksimts03@gmail.com>
2025-02-16 11:35:41 +0300

    `feat: Added conditional rendering of Yandex Metrika script based on environment`

--- src/components/YandexMetrika/YandexMetrika.tsx
+++ src/components/YandexMetrika/YandexMetrika.tsx
@@ -1,37 +1,42 @@
-// components/YandexMetrika.tsx
 import Script from 'next/script';
 
 export const YandexMetrika = () => {
+    const isProduction = process.env.NEXT_PUBLIC_ENV === 'production';
+
     return (
         <>
-            <Script
-                id="yandex-metrika"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-                        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                        m[i].l=1*new Date();
-                        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-                        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+            {isProduction && (
+                <>
+                    <Script
+                        id="yandex-metrika"
+                        strategy="afterInteractive"
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                                m[i].l=1*new Date();
+                                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-                        ym(99932809, "init", {
-                            clickmap:true,
-                            trackLinks:true,
-                            accurateTrackBounce:true
-                        });
-                    `,
-                }}
-            />
-            <noscript>
-                <div>
-                    <img
-                        src="https://mc.yandex.ru/watch/99932809"
-                        style={{ position: 'absolute', left: '-9999px' }}
-                        alt=""
+                                ym(99932809, "init", {
+                                    clickmap:true,
+                                    trackLinks:true,
+                                    accurateTrackBounce:true
+                                });
+                            `,
+                        }}
                     />
-                </div>
-            </noscript>
+                    <noscript>
+                        <div>
+                            <img
+                                src="https://mc.yandex.ru/watch/99932809"
+                                style={{ position: 'absolute', left: '-9999px' }}
+                                alt=""
+                            />
+                        </div>
+                    </noscript>
+                </>
+            )}
         </>
     );
 };
