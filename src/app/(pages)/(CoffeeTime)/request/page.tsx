eb1a2697efbcf8fe986891695c827ad35ad3c2cd
Mana Nedau <maksimts03@gmail.com>
2025-02-22 01:45:53 +0300

    `Added JobRequest component to RequestPage`

--- src/app/(pages)/(CoffeeTime)/request/page.tsx
+++ src/app/(pages)/(CoffeeTime)/request/page.tsx
@@ -1,10 +1,11 @@
-import { Htag, P } from "@/components"
+import { Htag, P, JobRequest } from "@/components"
 
 export default function RequestPage() {
     return (
         <div>
             <Htag tag='h1'>Заявка</Htag>
             <P>Здесь будет форма для отправки заявки</P>
+            <JobRequest />
         </div>
     )
 }
