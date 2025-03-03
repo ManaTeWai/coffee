5d4a63f95e7babbed3ea05a4e9b21606afa1fe5e
Mana Nedau <maksimts03@gmail.com>
2025-03-03 09:04:33 +0300

    `Updated Footer styles and icons in Footer.tsx`

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -41,9 +41,12 @@ export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
 					<div>
 						<Htag tag='h3' className={styles.menu_header}>Мы в социальных сетях</Htag>
 						<div className={styles.social}>
-							<Link href="https://vk.com/manatewai"><Image src="/img/vk.png" alt="вк" width={60} height={60} /></Link>
-							<Link href="https://wa.me/89885271618"><Image src="/img/whatsapp.png" alt="вотсап" width={40} height={40} /></Link>
-							<Link href="https://t.me/manatewai"><Image src="/img/tg.png" alt="телеграм" width={50} height={50} /></Link>
+							<Link href="https://vk.com/manatewai" className={styles.socials_item}>
+								<svg xmlns="http://www.w3.org/2000/svg" className={styles.socials_item_icon}><use href="./img/social.svg#i-vk"></use></svg>
+							</Link>
+							<Link href="https://t.me/manatewai" className={styles.socials_item}>
+								<svg xmlns="http://www.w3.org/2000/svg" className={styles.socials_item_icon}><use href="./img/social.svg#i-tg"></use></svg>
+							</Link>
 						</div>
 					</div>
 				</div>
