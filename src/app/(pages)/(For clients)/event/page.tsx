d18bce1a9e3c93fb13748a27835807724ccb34eb
Mana Nedau <maksimts03@gmail.com>
2025-03-23 11:57:47 +0300

    `Removed .jpg images and replaced with .png versions in promotion cards`

--- src/app/(pages)/(For clients)/event/page.tsx
+++ src/app/(pages)/(For clients)/event/page.tsx
@@ -14,19 +14,19 @@ export default function Event() {
 			<P size="large">Узнайте о наших выгодных предложениях и получите скидки на любимые напитки!</P>
 			<div className={styles.promotions}>
 				<div className={styles.promotion_card}>
-					<Image width={300} height={300} src="/img/promotion1.jpg" alt="Акция 1"/>
+					<Image width={300} height={300} src="/img/promotion1.png" alt="Акция 1"/>
 						<Htag tag="h2">Скидка 20% на все кофе</Htag>
 						<P size='medium'>Получите скидку 20% на весь ассортимент кофе. Акция действует до 31 октября.</P>
 						<Button appearance="primary">Участвовать</Button>
 				</div>
 				<div className={styles.promotion_card}>
-					<Image width={300} height={300} src="/img/promotion2.jpg" alt="Акция 2"/>
+					<Image width={300} height={300} src="/img/promotion2.png" alt="Акция 2"/>
 						<Htag tag='h2'>Бесплатный десерт</Htag>
 						<P size='medium'>Закажите два кофе и получите десерт в подарок. Акция действует только по выходным.</P>
 						<Button appearance='primary'>Участвовать</Button>
 				</div>
 				<div className={styles.promotion_card}>
-					<Image width={300} height={300} src="/img/promotion3.jpg" alt="Акция 3"/>
+					<Image width={300} height={300} src="/img/promotion3.png" alt="Акция 3"/>
 						<Htag tag='h2'>Счастливые часы</Htag>
 						<P size='medium'>С 14:00 до 16:00 получите скидку 30% на все напитки. Не упустите возможность!</P>
 						<Button appearance='primary'>Участвовать</Button>
